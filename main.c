77753ac53518247f2f8cccd7af5cba57285003e2
ChristianSAndersen <s164150@student.dtu.dk>
2018-04-28 16:36:58 +0200

    Added commits and cleaned up.

--- main.c
+++ main.c
@@ -17,11 +17,23 @@ int getInput() {
     return n;
 }
 
+/**
+ * Uses the equiation for the line, to check the y value of a particular point of a segment.
+ * @param x - The point on our segment we want to know the y value for.
+ * @param current - The segment we will be checking.
+ * @return - Returns the y value for the segment.
+ */
 int getY(int x, segment current) {
     int a = (abs(current.endy - current.starty)/abs(current.startx - current.endx));
     return (current.endy + a*(x - current.endx));
 }
 
+/**
+ * Checks how much a segment is blocked by another segment.
+ * @param current - Segment we want to check for how much it is blocked.
+ * @param compare - Segment which maybe blocks our current segment.
+ * @return The amount our current segment is blocked by the compared segment.
+ */
 int amountBlocked(segment current, segment compare) {
     if (current.starty < compare.starty || current.starty < compare.endy || current.endy < compare.starty || current.endy < compare.endy) {
         if (current.startx >= compare.startx && current.endx <= compare.endx) {
@@ -49,34 +61,47 @@ int amountBlocked(segment current, segment compare) {
     return 0;
 }
 
-void findBlocked(segment * segments, int length, int current) {
-    int original = segments[current].capacity;
+/**
+ * Checks every segment, if another segment blocks it.
+ * @param segments - All roof segments.
+ * @param length - Amount of segments in the array.
+ */
+void findBlocked(segment * segments, int length) {
     for (int i = 0 ; i < length ; i++) {
-        if (i != current) {
-            int n = amountBlocked(segments[current], segments[i]);
-            if ((original - n) < segments[current].capacity) {
-                segments[current].capacity = (original - n);
+        int original = segments[i].capacity;
+        for (int j = 0 ; j < length ; j++) {
+            if (j != i) {
+                int n = amountBlocked(segments[i], segments[i]);
+                if ((original - n) < segments[i].capacity) {
+                    segments[i].capacity = (original - n);
+                }
             }
         }
     }
 }
 
-int checkValidFall(segment current, segment compare) { //Returns weather or not the current segment falls on the segment it compares to.
-    if (current.falldir && compare.falldir) { //Both segments fall left.
+/**
+ * Returns weather or not the current segment falls on the segment it compares to.
+ * @param current - The segment we're checking if it falls on another.
+ * @param compare - The segment we would like to know if another falls on.
+ * @return 1 if it falls on it, 0 if it does not.
+ */
+int checkValidFall(segment current, segment compare) {
+    if (current.falldir && compare.falldir) {                                       //Both segments fall left.
         if (current.startx <= compare.endx && current.startx >= compare.startx) {
             int comparey = getY(current.startx, compare);
             if (comparey < current.starty) {
                 return 1;
             }
         }
-    } else if (current.falldir && !compare.falldir) { //Current falls left, compare falls right.
+    } else if (current.falldir && !compare.falldir) {                               //Current falls left, compare falls right.
         if (current.startx <= compare.endx && current.startx >= compare.startx) {
             int comparey = getY(current.startx, compare);
             if (comparey < current.starty) {
                 return 1;
             }
         }
-    } else if (!current.falldir && compare.falldir) { //Current falls right, compare falls left.
+    } else if (!current.falldir && compare.falldir) {                               //Current falls right, compare falls left.
         if (current.endx <= compare.endx && current.endx >= compare.startx) {
             int comparey = getY(current.endx, compare);
             if (comparey < current.endy) {
@@ -84,7 +109,7 @@ int checkValidFall(segment current, segment compare) { //Returns weather or not 
             }
         }
     } else { //Both fall right.
-        if (current.endx <= compare.endx && current.endx >= compare.startx) { //End point x value is between compare start and end point.
+        if (current.endx <= compare.endx && current.endx >= compare.startx) {       //End point x value is between compare start and end point.
             int comparey = getY(current.endx, compare);
             if (comparey < current.endy) {
                 return 1;
@@ -94,18 +119,25 @@ int checkValidFall(segment current, segment compare) { //Returns weather or not 
     return 0;
 }
 
-void findFallsOn(segment * segments, int length, int current) { //Cycles through all combinations of two segments, and finds out which fall on which.
+/**
+ * Cycles through all roof segments, to check if any of them delivers water to another piece. Also checks if the segment to compare to is itself.
+ * @param segments - The segments for the roof.
+ * @param length - The total amount of segments.
+ */
+void findFallsOn(segment * segments, int length) {
     for (int i = 0 ; i < length ; i++) {
-        if (i != current) { //Doesn't compare a segment with itself.
-            if (checkValidFall(segments[current], segments[i])) {
-                segments[current].fallsOn = &segments[i];
-                break;
+        for (int j = 0 ; j < length ; j++) {
+            if (j != i) {
+                if (checkValidFall(segments[i], segments[j])) {
+                    segments[i].fallsOn = &segments[j];
+                    break;
+                }
             }
         }
     }
 }
 
-segment initSegment() { //Initializes each segment, one at a time.
+segment initSegment() {
     int input[4];
     scanf("%d %d %d %d", &input[0], &input[1], &input[2], &input[3]);
     struct segment segment;
@@ -123,21 +155,17 @@ segment initSegment() { //Initializes each segment, one at a time.
     return segment;
 }
 
-void printSegments(segment * segments, int length) {
-    for (int i = 0 ; i < length ; i++) {
-        printf("%p %d %d %d %d %d %d %p\n", &segments[i], segments[i].startx, segments[i].starty, segments[i].endx, segments[i].endy, segments[i].falldir, segments[i].capacity, segments[i].fallsOn);
-    }
-}
+int main() {
 
-int main(void) {
-    /*
-    int length = getInput(); //Variable for number of segments.
+    //Variable for number of segments.
+    int length = getInput();
     segment segments[length];
-    for (int i = 0 ; i < length ; i++) { //Loads each segment, one at a time.
+    //Loads each segment, one at a time.
+    for (int i = 0 ; i < length ; i++) {
         segments[i] = initSegment();
     }
-    */
 
+    /* Used to test with. Value each segment should return is right of every line.
     int length = 6;
     segment segments[] = {
             {13, 7, 15, 6, 0, abs(13-15)},  //2
@@ -147,25 +175,20 @@ int main(void) {
             {6, 3, 8, 2, 0, abs(6-8)},      //0
             {9, 6, 12, 8, 1, abs(9-12)}     //3
     };
-    /*
-    int length = 2;
-    segment segments[] = {
-            {8, 9, 10, 7, 0, abs(8-10)},  //0
-            {4, 6, 11, 13, 0, abs(4-11)}     //7
-    };
-     */
-    for (int i = 0 ; i < length ; i++) { //First checks if any of the segments contribute to another segments total capacity.
-        findFallsOn(segments, length, i);
-    }
-    for (int i = 0 ; i < length ; i++) { //Checks if a segment is blocked by another segment.
-        findBlocked(segments, length, i);
-    }
-    for (int i = 0 ; i < length ; i++) { //Updates each segments capacity, with regards to what falls on it from other segments.
+    */
+
+    //First checks if any of the segments deliver water to another segment.
+    findFallsOn(segments, length);
+    //Second, checks if any segments receive less water, because it is blocked by another segment.
+    findBlocked(segments, length);
+    //Third, update each segments capacity, with regards to what falls on it from other segments.
+    for (int i = 0 ; i < length ; i++) {
         if (segments[i].fallsOn) {
             segments[i].fallsOn->capacity += segments[i].capacity;
         }
     }
-    for (int i = 0 ; i < length ; i++) { //Prints the capacity of each segment in order they were inputted.
+    //Last, print the capacity of each segment in order they were entered.
+    for (int i = 0 ; i < length ; i++) {
         printf("%d\n", segments[i].capacity);
     }
     return 0;
